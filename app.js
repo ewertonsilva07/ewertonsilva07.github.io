903feeb3a3cf7c3fa9189a2e15ce8a61a010027f
Ewerton <ewerton.silva07@hotmail.com>
2023-12-22 13:51:21 -0300

    fix the main page transitions

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
-const greetingMsg = document.querySelector('#greeting-msg');
-const selfIntroduction = document.querySelector('.self-introduction');
-const codeSectionTitle = document.querySelector('h3:nth-of-type(1)');
-const designSectionTitle = document.querySelector('h3:nth-of-type(2)');
-const mySkillsTitle = document.querySelector('#abilities h2');
+const selectLogos = {
+    greetingMsg : document.querySelector('#greeting-msg'),
+    selfIntroduction : document.querySelector('.self-introduction'),
+    abilitiesSection : document.querySelector('#abilities'),
+    codeSectionTitle : document.querySelector('h3:nth-of-type(1)'),
+    designSectionTitle : document.querySelector('h3:nth-of-type(2)'),
+    mySkillsTitle : document.querySelector('#abilities h2'),
+    htmlIcon : document.querySelector('#code i:nth-of-type(1)'), 
+    cssIcon : document.querySelector('#code i:nth-of-type(2)'), 
+    javascriptIcon : document.querySelector('#code i:nth-of-type(3)'), 
+    bootstrapIcon : document.querySelector('#code i:nth-of-type(4)'), 
+    gitIcon : document.querySelector('#code i:nth-of-type(5)'), 
+    gitHubIcon : document.querySelector('#code i:nth-of-type(6)'), 
+    photoshopIcon : document.querySelector('#design i:nth-of-type(1)'),
+    figmaIcon : document.querySelector('#design i:nth-of-type(2)'),
+    premiereIcon : document.querySelector('#design i:nth-of-type(3)')
+}
 
 
 const contentTransition = function(el, classToggle, delay) {
@@ -14,35 +26,24 @@ const contentTransition = function(el, classToggle, delay) {
     })
 }
 
-const htmlIcon = document.querySelector('#code i:nth-of-type(1)'); 
-const cssIcon = document.querySelector('#code i:nth-of-type(2)'); 
-const javascriptIcon = document.querySelector('#code i:nth-of-type(3)'); 
-const bootstrapIcon = document.querySelector('#code i:nth-of-type(4)'); 
-const gitIcon = document.querySelector('#code i:nth-of-type(5)'); 
-const gitHubIcon = document.querySelector('#code i:nth-of-type(6)'); 
-
-const photoshopIcon = document.querySelector('#design i:nth-of-type(1)');
-const figmaIcon = document.querySelector('#design i:nth-of-type(2)');
-const premiereIcon = document.querySelector('#design i:nth-of-type(3)');
-
 
 async function transition(){
-    await contentTransition(greetingMsg,'show-page-content', 3000);
-    await contentTransition(greetingMsg,'show-page-content', 3000);
-    await contentTransition(selfIntroduction,'show-page-content', 1000);
-    await contentTransition(mySkillsTitle, 'show-page-content', 1000);
-    await contentTransition(codeSectionTitle,'show-page-content', 1000);
-    await contentTransition(htmlIcon,'show-page-content', 300);
-    await contentTransition(cssIcon, 'show-page-content', 300);
-    await contentTransition(javascriptIcon, 'show-page-content', 300);
-    await contentTransition(bootstrapIcon, 'show-page-content', 300);
-    await contentTransition(gitIcon, 'show-page-content', 300);
-    await contentTransition(gitHubIcon, 'show-page-content', 300);
-    await contentTransition(designSectionTitle, 'show-page-content', 500);
-    await contentTransition(photoshopIcon, 'show-page-content', 300);
-    await contentTransition(figmaIcon, 'show-page-content', 300);
-    await contentTransition(premiereIcon, 'show-page-content', 300);
-
+    await contentTransition(selectLogos.greetingMsg,'show-page-content', 1000);
+    await contentTransition(selectLogos.greetingMsg,'show-page-content', 3000);
+    await contentTransition(selectLogos.selfIntroduction,'show-page-content', 1000);
+    await contentTransition(selectLogos.abilitiesSection, 'show-page-content', 1000);
+    await contentTransition(selectLogos.mySkillsTitle, 'show-page-content', 500);
+    await contentTransition(selectLogos.codeSectionTitle,'show-page-content', 1000);
+    await contentTransition(selectLogos.htmlIcon,'show-page-content', 300);
+    await contentTransition(selectLogos.cssIcon, 'show-page-content', 300);
+    await contentTransition(selectLogos.javascriptIcon, 'show-page-content', 300);
+    await contentTransition(selectLogos.bootstrapIcon, 'show-page-content', 300);
+    await contentTransition(selectLogos.gitIcon, 'show-page-content', 300);
+    await contentTransition(selectLogos.gitHubIcon, 'show-page-content', 300);
+    await contentTransition(selectLogos.designSectionTitle, 'show-page-content', 500);
+    await contentTransition(selectLogos.photoshopIcon, 'show-page-content', 300);
+    await contentTransition(selectLogos.figmaIcon, 'show-page-content', 300);
+    await contentTransition(selectLogos.premiereIcon, 'show-page-content', 300);
 }
 
 transition();
