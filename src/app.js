b9f5484519bc5813c750c6ac4db325298430770d
Ewerton <ewerton.silva07@hotmail.com>
2024-02-28 16:13:26 -0300

    use OOP on the selectElements object

--- src/app.js
+++ src/app.js
@@ -1,13 +1,38 @@
+const _element = new WeakMap();
 
-const selectElements = {
-    greetingMsg : document.querySelector('#greeting-msg'),
-    loadingBar: document.querySelector('.loading-bar'),
-    selfIntroduction : document.querySelector('.self-introduction'),
-    abilitiesSection : document.querySelector('#abilities'),
-    codeSectionTitle : document.querySelector('h3:nth-of-type(1)'),
-    designSectionTitle : document.querySelector('h3:nth-of-type(2)'),
-    mySkillsTitle : document.querySelector('#abilities h2')
+class getPageElement{
+    constructor(element){
+        _element.set(this, element);
+    }
+
+    get element(){
+        return _element.get(this);
+    }
+
+    selectElement(){
+        return document.querySelector(_element.get(this))
+    }
 }
+
+const greetingMsg = new getPageElement('#greeting-msg');
+const loadingBar = new getPageElement('.loading-bar');
+const selfIntroduction = new getPageElement('.self-introduction');
+const abilitiesSection = new getPageElement('#abilities');
+const codeSectionTitle = new getPageElement('h3:nth-of-type(1)');
+const designSectionTitle = new getPageElement('h3:nth-of-type(2)');
+const mySkillsTitle = new getPageElement('#abilities h2');
+
+
+
+// const selectElements = {
+//     greetingMsg : document.querySelector('#greeting-msg'),
+//     loadingBar: document.querySelector('.loading-bar'),
+//     selfIntroduction : document.querySelector('.self-introduction'),
+//     abilitiesSection : document.querySelector('#abilities'),
+//     codeSectionTitle : document.querySelector('h3:nth-of-type(1)'),
+//     designSectionTitle : document.querySelector('h3:nth-of-type(2)'),
+//     mySkillsTitle : document.querySelector('#abilities h2')
+// }
 
 const selectCodeLogos = {
     htmlIcon : document.querySelector('#code .my-skill-card:nth-of-type(1)'), 
@@ -35,13 +60,13 @@ const contentTransition = function(el, classToggle, delay) {
 
 
 async function transition(){
-    await contentTransition(selectElements.greetingMsg,'show-page-content', 1000);
-    await contentTransition(selectElements.loadingBar,'loading-bar-fill', 200);
-    await contentTransition(selectElements.greetingMsg,'show-page-content', 3000);
-    await contentTransition(selectElements.selfIntroduction,'show-page-content', 1000);
-    await contentTransition(selectElements.abilitiesSection, 'show-page-content', 1000);
-    await contentTransition(selectElements.mySkillsTitle, 'show-page-content', 500);
-    await contentTransition(selectElements.codeSectionTitle,'show-page-content', 1000);
+    await contentTransition(greetingMsg.selectElement(),'show-page-content', 1000);
+    await contentTransition(loadingBar.selectElement(),'loading-bar-fill', 200);
+    await contentTransition(greetingMsg.selectElement(),'show-page-content', 3000);
+    await contentTransition(selfIntroduction.selectElement(),'show-page-content', 1000);
+    await contentTransition(abilitiesSection.selectElement(), 'show-page-content', 1000);
+    await contentTransition(mySkillsTitle.selectElement(), 'show-page-content', 500);
+    await contentTransition(codeSectionTitle.selectElement(),'show-page-content', 1000);
     await contentTransition(selectCodeLogos.htmlIcon,'show-page-content', 300);
     await contentTransition(selectCodeLogos.cssIcon, 'show-page-content', 300);
     await contentTransition(selectCodeLogos.javascriptIcon, 'show-page-content', 300);
